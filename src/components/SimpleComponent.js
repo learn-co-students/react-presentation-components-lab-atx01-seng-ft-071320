704c1a88a43278d758a78b9f9b6af82f5b43363f
Ceddyp19 <ceddyp117@gmail.com>
2020-09-18 14:06:34 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,16 @@
+//const { ContextReplacementPlugin } = require("webpack");
+import React, { Component } from 'react';
+
 // Code SimpleComponent Here
+
+class SimpleComponent extends Component {
+    state = { mood: 'happy' }
+
+    render() { 
+    return ( <div onClick={this.handleClick}>{this.state.mood}</div> );
+    }
+
+    handleClick = () =>  this.setState({mood: 'sad'})
+}
+ 
+export default SimpleComponent;
